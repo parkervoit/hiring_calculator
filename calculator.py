ccf2599640d099a2db0e09af082f16082736c6d3
Parker Voit <pvoit@skipcart.com>
2022-03-21 12:46:03 -0500

    bugfix

--- calculator.py
+++ calculator.py
@@ -3,22 +3,22 @@ import streamlit as st
 st.title("Applicant Calculator")
 with st.form("app_calculator"):
     attrition_rate = st.number_input(label = 'Attrition Rate', 
-                                    min_value = 0, 
-                                    max_value = 1,
-                                    step = .02)
+                                    min_value = float(0), 
+                                    max_value = float(1),
+                                    step = float(.02))
     total_orders = st.number_input(label = 'Total Orders', 
-                                    min_value = 0,
-                                    step = 1)
+                                    min_value = float(0),
+                                    step = float(1))
     drivers = st.number_input(label = 'Drivers', 
-                                    min_value = 0,
-                                    step = 1)
+                                    min_value = float(0),
+                                    step = float(1))
     order_rate = st.number_input(label = 'Order Rate', 
-                                    min_value = 0,
-                                    step = .25)
+                                    min_value = float(0),
+                                    step = float(.25))
     conversion_rate = st.number_input(label = 'Conversion Rate4', 
-                                    min_value = 0,
-                                    max_value = 1,
-                                    step = .02)
+                                    min_value = float(0),
+                                    max_value = float(1),
+                                    step = float(.02))
 
     model = st.selectbox("Pick your model", options = ['New Market','Existing Market'])
     submitted = st.form_submit_button("Calculate")
